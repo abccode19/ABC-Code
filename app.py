44c632705793368306f059d85c2c617c0a345644
ABC-CODE <98007853+abccode19@users.noreply.github.com>
2022-02-05 11:53:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 @app.route('/')
 def index():
 	return render_template('index.html')
+@app.route('/home')
+def home():
+	return render_template('home.html')
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
+@app.route('/developer')
+def developer():
+	return render_template('developer.html')
+@app.route('/about')
+def about():
+	return render_template('about.html')
+@app.route('/createcourse')
+def create():
+	return render_template('createcourse.html')
 if __name__=='__main__':
-	app.run(debug=False ,port=8000)
+	app.run(debug=True)
 	
