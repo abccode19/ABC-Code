e71c3d6e0bf130ab29cf8ff8b21543e69719a040
ABC-CODE <98007853+abccode19@users.noreply.github.com>
2022-02-05 11:57:02 +0530

    Add files via upload

---
+++ static/js.js
@@ -0,0 +1,16 @@
+function slide(){
+    document.getElementById('sidebar')
+    .classList.toggle('active');
+ 
+    
+}
+function help(){
+				 document.getElementById('how-to-upload')
+    .classList.toggle('active');
+  up_down= document.getElementById('down');
+  
+ up_down.classList.toggle('active')  ;
+       
+     }
+
+
